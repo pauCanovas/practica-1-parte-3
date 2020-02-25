e10e72dd68ac18b4bf6763e0eae9309d4a1f8943
pauCanovas <55945537+pauCanovas@users.noreply.github.com>
2020-02-25 19:39:51 +0100

    bases

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Salvar el Pangolín</title>
+    </head>
+    <body>
+        <?php include 'menu.php';?>
+        
+    </body>
+</html>
