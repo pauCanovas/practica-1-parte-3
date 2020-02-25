915fc525704f3d477ee64ea977bbc6ad6b675b1f
sfernandez342 <56076729+sfernandez342@users.noreply.github.com>
2020-02-25 19:42:48 +0100

    menu + css

---
+++ menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Inicial</a></li>
+                <li><a href="#">Información Generica</a></li>
+                <li><a href="#">Documentos</a></li>
+                <li><a href="#">Pagina de contacto</a></li>
+                <li><a href="#">Condiciones legales</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
