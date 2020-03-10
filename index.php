bef8e46bf73583a646660d397563734a3870bb87
pauCanovas <55945537+pauCanovas@users.noreply.github.com>
2020-03-10 20:30:53 +0100

    cambios index

--- index.php
+++ index.php
@@ -11,16 +11,23 @@
             <?php require_once 'menu.php';?>
             <div id="contenido">
                 <h1>Salvar al pangolín</h1>
+                <h3>
+                    Un estudio por Pau Cánovas Nuevo y Sergio Fernandez Aguilera
+                </h3>
                 <div>
-                    El pangolín es una especie en peligro de extincion
+                    Esta pagina web esta dedicada a hablar de el trafico de el pangolin
                 </div>
                 <div>
                     <img class="img" id="pangolin1" src="img/pangolin1.jpeg" alt="Imagen de un pangolín"/>
                     <img class="img" id="pangolin2" src="img/pangolin2.jpg" alt="Imagen de un pangolín"/>
                     <img class="img" id="pangolin3" src="img/pangolin3.jpg" alt="Imagen de un pangolín"/>
                 </div>
+                <br>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/w7JB2gP4gXM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
+            
         </div>
     </body>
+    
+
 </html>
