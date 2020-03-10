2fa8266a49f368fb855aa9e27a0b97ada5b0ba94
pauCanovas <55945537+pauCanovas@users.noreply.github.com>
2020-03-10 20:58:35 +0100

    cambio logo imagen

--- downloads.php
+++ downloads.php
@@ -12,7 +12,7 @@
                 <ul>
                     <li><a title="Document odt" href="descargas/eigrp.odt"><img class="logo" src="img/document-logo.png" alt="redes" />Redes</a></li>
                     <li><a title="Document pdf" href="descargas/horari.pdf"><img class="logo" src="img/pdf-logo.png" alt="Documento pdf" />Horario DAW</a></li>
-                    <li><a title="Imagen Pangolín" href="descargas/Pangolin3.jpg"><img class="logo" src="img/imagen-logo.jpg" alt="Imagen Pangolín" />Imagen Pangolín</a></li>
+                    <li><a title="Imagen Pangolín" href="descargas/Pangolin3.jpg"><img class="logo" src="img/logo-imagen.png" alt="Imagen Pangolín" />Imagen Pangolín</a></li>
                     <li><a title="Tecla piano" href="descargas/a1.mp3"><img class="logo" src="img/mp3-logo.png" alt="Tecla piano" />Tecla piano</a></li>
                 </ul>
             </div>
