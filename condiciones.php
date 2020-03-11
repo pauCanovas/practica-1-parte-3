a2b2107fa7c4e48eb9d736005b04bca0f2042a2a
pauCanovas <55945537+pauCanovas@users.noreply.github.com>
2020-03-11 17:48:55 +0100

    Condiciones legales

--- condiciones.php
+++ condiciones.php
@@ -8,6 +8,35 @@
     <body>
         <div id="wrapper">
             <?php include 'menu.php';?>
+            <div id="texto">
+                <p>
+                    1. RESPONSABLE DEL TRATAMIENTO Y DELEGADO DE PROTECCIÓN DE DATOS PERSONALES
+
+                    Salvar al Pangolín le informa de que el responsable del tratamiento de los datos de carácter personal que se recojan al contactar con nosotros es RobaInformacion, S.L. (en adelante, Roba), con domicilio social en Calle Falsa 123, nº 8, 123456 Plumbergia, y con número de identificación fiscal B-2.123.456.
+                    Asimismo, le informamos de que en caso de que Vd. desee ponerse en contacto con nuestro DPO para formular cualquier consulta o duda, envíenos un correo electrónico a: salvapangolin@pangolon.com.
+                </p>
+                <p>
+                    2. FINALIDADES DEL TRATAMIENTO Y LEGITIMACIÓN
+
+                    De conformidad con la normativa vigente en materia de protección de datos, Salvar al Pangolín le informa de que sus datos personales serán recabados y tratados con la finalidad de:
+
+                    Gestionar y responder a las dudas o consultas que se planteen, cualquier petición que Vd. realice.
+                    Asimismo, y siempre y cuando nos haya proporcionado su consentimiento mediante la marcación de la respectiva casilla podremos tratar sus datos para remitirle newsletters informativas de Salvar animales, que serán siempre personalizadas y adaptadas a sus intereses.
+                    Le informamos de que el tratamiento es legítimo porque Vd. ha prestado su consentimiento.
+                </p>
+                <p>
+                    3. CRITERIOS DE CONSERVACIÓN DE LOS DATOS PERSONALES
+
+                    Salvar al Pangolin le informa de que conservaremos sus datos personales mientras Vd. no revoque el consentimiento prestado para su tratamiento o durante los años necesarios para cumplir con las obligaciones legales.
+                </p>
+                <p>
+                    4. DERECHOS DEL TITULAR DE LOS DATOS
+
+                    Vd. puede ejercitar sus derechos de acceso, rectificación, supresión, limitación del tratamiento, portabilidad de los datos y oposición ante el responsable del tratamiento dirigiendo su solicitud por escrito con la referencia "Datos Personales" a C/ Provençals nº 39, 08019, Barcelona o mediante el envío de un correo electrónico con la misma referencia a protecciondatos@pangolin.com especificando que derecho desea ejercitar.
+
+                    Le informamos de que podrá ejercer su derecho de reclamación ante la autoridad de control competente, siendo en España la Agencia Española de Protección de Datos en www.agpd.es.
+                </p>
+            </div>
         </div>
     </body>
 </html>
