8ec4e1b084c2575d9dcdeb3bf1f774c3bf6daa09
pauCanovas <55945537+pauCanovas@users.noreply.github.com>
2020-03-11 17:18:13 +0100

    fix imagenes
    arreglar orden de imagenes y poner alt

--- downloads.php
+++ downloads.php
@@ -8,12 +8,12 @@
     <body>
         <div id="wrapper">
             <?php include 'menu.php';?>
-            <div id="texto">
+            <div style="padding-top: 120px;">
                 <ul>
-                    <li><a title="Document odt" href="descargas/eigrp.odt"><img class="logo" src="img/document-logo.png" alt="redes" />Redes</a></li>
+                    <li><a title="Document odt" href="descargas/eigrp.odt"><img class="logo" src="img/document-logo.png" alt="documento odt" />Redes</a></li>
                     <li><a title="Document pdf" href="descargas/horari.pdf"><img class="logo" src="img/pdf-logo.png" alt="Documento pdf" />Horario DAW</a></li>
-                    <li><a title="Imagen Pangolín" href="descargas/Pangolin3.jpg"><img class="logo" src="img/logo-imagen.png" alt="Imagen Pangolín" />Imagen Pangolín</a></li>
-                    <li><a title="Tecla piano" href="descargas/a1.mp3"><img class="logo" src="img/mp3-logo.png" alt="Tecla piano" />Tecla piano</a></li>
+                    <li><a title="Imagen Pangolín" href="descargas/Pangolin3.jpg"><img class="logo" src="img/logo-imagen.png" alt="imagen png" />Imagen Pangolín</a></li>
+                    <li><a title="Tecla piano" href="descargas/a1.mp3"><img class="logo" src="img/mp3-logo.png" alt="sonido mp3" />Tecla piano</a></li>
                 </ul>
             </div>
         </div>
